d2cd2c6e9efe210de1778b5cfbebe4723be74785
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-03-10 18:32:36 -0400

    Update Info-Host.js

--- plugins/Info-Host.js
+++ plugins/Info-Host.js
@@ -1,64 +1,24 @@
-let { performance } = require('perf_hooks')
-let fs = require ('fs')
-let path = require('path')
-let handler  = async (m, { conn, usedPrefix }) => { 
-  let package = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json')))
-  let _uptime = process.uptime() * 1000
-  let uptime = clockString(_uptime) 
-  let totalreg = Object.keys(global.DATABASE._data.users).length
-  let old = Math.round(performance.now())
-  await m.reply('*_CUALQUIER DUDA O AYUDA QUE NECESITES CONTACTEME CON EL SIGUIENTE LINK: wa.me/14509001764_*')
-  let neww = Math.round(performance.now())
-  conn.reply(m.chat, `
-*_GRACIAS POR INTERESARTE EN INSTALAR THE SHADOW BROKERS - BOT_*
+const handler = async (m, {conn}) => {
+  m.reply(global.terminos);
+};
+handler.customPrefix = /infohost|host|Host|cafirexos|Cafirexos/i;
+handler.command = new RegExp;
+export default handler;
 
-*◄┢┅͜͡✇⟬↯ື ►ஜ۩💥۩ஜ◄ ↯ື⟭✇͜͡┅┧►*
+global.terminos = `
+*_Hola a todos 👋🏻 ¡Estamos emocionados de anunciar nuestra nueva colaboración con Cafirexos  🤖 Ahora ya pueden tener su propio bot de *THE-MYSTIC-BOT-MD* en su plataforma en línea, permitiendo  que tus bots estén activos las 24/7. 💻_
 
-_VIDEO TUTORIAL DE INSTALACIÓN: https://youtu.be/dP8-aaHinBE_
+_Te quería informarte que la instalación de estas versiones ha sido aprobada y ya está en pleno funcionamiento. Además, *los servidores son totalmente compatibles, lo que te permite elegir el tipo de inicio del bot según tus necesidades.* ¡Prepárate para una experiencia de usuario ininterrumpida y emocionante!_ 
 
-_ARCHIVOS DEL BOT: En la despcripcion del video encuentras el enlace actualizado, link directo a mediafire_
+🖥️ *Plataforma:*
+https://dash.cafirexos.com/login
 
-_^‿^ REQUISITO PARA INSTALAR EL BOT ^‿^_
-_( ͡° ͜ʖ ͡°) Termux_
-_( ͡° ͜ʖ ͡°) Descomprimidor de archivos (recomiendo ZArchiver)_
-_( ͡° ͜ʖ ͡°) 1.5 GB de memoria en el teléfono_
-_( ͡° ͜ʖ ͡°) Un WhatsApp secundario_
-_( ͡° ͜ʖ ͡°) 2 teléfonos o un 1 teléfono y una PC_
+🧑🏻‍💻 *Cualquier Información entra a este grupo:*
+https://chat.whatsapp.com/FBtyc8Q5w2iJXVl5zGJdFJ
 
-*◄┢┅͜͡✇⟬↯ື ►ஜ۩💥۩ஜ◄ ↯ື⟭✇͜͡┅┧►*
+🔮 *Canal:*
+https://www.whatsapp.com/channel/0029Va8ZD6O3mFXxTPl1m13A 
 
-💥 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐃𝐄 𝐈𝐍𝐒𝐓𝐀𝐋𝐀𝐂𝐈𝐎𝐍 💥
-- pkg update && pkg upgrade
-- pkg install git -y
-- pkg install nodejs -y
-- pkg install ffmpeg -y
-- pkg install imagemagick -y
-- termux-setup-storage
-- cd storage/downloads/Shadow
-- npm install
-- npm install -g npm@8.1.4
-- npm update
-- npm start
-° Escaneas el código QR tienes 30 segundos para hacerlo
-
-*◄┢┅͜͡✇⟬↯ື ►ஜ۩💥۩ஜ◄ ↯ື⟭✇͜͡┅┧►*
-`.trim(), m)
-}
-handler.help = ['instalarbot']
-handler.tags = ['General']
-handler.command = /^(instalarbot?)$/i
-
-handler.fail = null
-
-module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-function clockString(ms) {
-  let h = Math.floor(ms / 3600000)
-  let m = Math.floor(ms / 60000) % 60
-  let s = Math.floor(ms / 1000) % 60
-  console.log({ms,h,m,s})
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
-}
+🤖 *Comuníquese con:*
+ > wa.me/50497160165
+  
